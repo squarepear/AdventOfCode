115039110d931ff7efa8d6be49db3bae5bd702d5
Jeffrey Harmon <16364318+SquarePear@users.noreply.github.com>
2021-12-07 00:22:09 -0500

    2021: Day seven complete!

---
+++ 2021/Day7/python/Part2/__main__.py
@@ -0,0 +1,31 @@
+def main():
+    file = open("input.txt")
+
+    lines = file.readlines()
+
+    result = 0
+
+    data = lines[0].strip().split(',')
+
+    val = (-1, 0)
+
+    for x in range(2000):
+        fuel = 0
+        for i in range(len(data)):
+            dist = abs(int(data[i]) - x)
+
+            fuel += round(dist * (dist + 1) / 2)
+
+        if fuel < val[1] or val[0] == -1:
+            val = (x, fuel)
+
+    result = val[1]
+
+    out = open("output-part2.txt", "w")
+    out.write(str(result))
+    file.close()
+    out.close()
+
+
+if (__name__ == "__main__"):
+    main()
